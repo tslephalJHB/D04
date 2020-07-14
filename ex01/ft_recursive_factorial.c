e3d29168dfe0a4e3e5182b9d842d3f116130b1f7
Tshiamo Lephale <tslephal@c4r9s1.wethinkcode.co.za>
2020-07-14 09:07:03 +0200

    Addition of ft_recursive_factorial.c

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tslephal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 09:05:00 by tslephal          #+#    #+#             */
+/*   Updated: 2020/07/14 09:05:22 by tslephal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_recursive_factorial(int nb)
+{
+    int factorial;
+    
+    factorial = 1;
+    if (nb > 0 && nb < 13)
+    {
+        factorial = factorial * nb;
+        return(factorial * ft_recursive_factorial(nb - 1));
+    }
+    else if (nb == 0)
+    {
+        return (1);
+    }
+    else
+    {
+        return(0);
+    }
+}
